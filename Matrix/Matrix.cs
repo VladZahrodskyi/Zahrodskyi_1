bc72bd26ce9a4b02aa90adeb0f2e614ac306b6b8
Vladyslav Zahrodskyi <v.zahrodskyi@gmail.com>
2019-01-10 00:07:59 +0200

    Add project files.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MatrixBuild
+{
+	public class OutOfRangOfMAtrixException : ApplicationException
+	{
+		public OutOfRangOfMAtrixException():base()
+		{ }
+		
+		public OutOfRangOfMAtrixException(string message) : base(message)
+		{ }
+
+		public OutOfRangOfMAtrixException(string message, Exception innerException) : base(message, innerException)
+		{ }
+		protected OutOfRangOfMAtrixException(SerializationInfo info, StreamingContext context):base(info, context)
+		{ }
+	}
+	public class NotSquaredMatrix : ApplicationException
+	{
+		public NotSquaredMatrix() : base()
+		{ }
+
+		public NotSquaredMatrix(string message) : base(message)
+		{ }
+
+		public NotSquaredMatrix(string message, Exception innerException) : base(message, innerException)
+		{ }
+		protected NotSquaredMatrix(SerializationInfo info, StreamingContext context) : base(info, context)
+		{ }
+	}
+
+		public class NotTheSameDimensionsMatrixs : ApplicationException
+	{
+		public NotTheSameDimensionsMatrixs() : base()
+		{ }
+
+		public NotTheSameDimensionsMatrixs(string message) : base(message)
+		{ }
+
+		public NotTheSameDimensionsMatrixs(string message, Exception innerException) : base(message, innerException)
+		{ }
+		protected NotTheSameDimensionsMatrixs(SerializationInfo info, StreamingContext context) : base(info, context)
+		{ }
+	}
+	public class ColumnsNotEquelRows : ApplicationException
+	{
+		public ColumnsNotEquelRows() : base()
+		{ }
+
+		public ColumnsNotEquelRows(string message) : base(message)
+		{ }
+
+		public ColumnsNotEquelRows(string message, Exception innerException) : base(message, innerException)
+		{ }
+		protected ColumnsNotEquelRows(SerializationInfo info, StreamingContext context) : base(info, context)
+		{ }
+	}
+	
+	public class Matrix
+	{
+		private double[,] _arrayForMatrix;
+
+		public int Rows { get; private set; }//definition only in ctor
+		public int Colums { get; private set; }//definition only in ctor
+
+		public Matrix() : this(5, 5)//goto main matrix ctor
+		{
+
+		}
+
+		public Matrix(int rows) : this(rows, 0)//vector matrix ctor
+		{
+
+		}
+
+		public Matrix(int rows, int colums)//main matrix ctor
+		{
+			Colums = colums;
+			Rows = rows;
+			_arrayForMatrix = new double[rows, colums];
+		}
+
+		public Matrix(Matrix matrix)
+		{
+			this.Colums = matrix.Colums;
+			this.Rows = matrix.Rows;
+			this._arrayForMatrix = matrix._arrayForMatrix;
+		}
+
+		public Matrix(int[] array) : this( 0 , array.Length)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				this._arrayForMatrix[0,i] = array[i];
+			}
+		}
+
+		public Matrix(int[,] array) : this((int)(array.LongLength/array.Length), array.Length)
+		{ 
+			for (int i = 0; i < array.Length; i++)
+			{
+				for (int j = 0; j < (int)(array.LongLength / array.Length); j++)
+				{
+					this._arrayForMatrix[j, i] = array[j,i];
+				}
+				
+			}
+		}
+
+		public double this[int rows, int colums]
+		{
+			get
+			{
+				if (rows < 0 || rows > Rows)
+				{
+					throw new OutOfRangOfMAtrixException("The Row is out of range!");
+				}
+				if (colums < 0 || colums > Colums)
+				{
+					throw new OutOfRangOfMAtrixException("The Colum is out of range!");
+				}
+				return _arrayForMatrix[rows, colums];
+			}
+			set { _arrayForMatrix[rows, colums] = value; }
+		}
+
+		public double[,] GetDoubleArray()
+		{
+			return _arrayForMatrix;
+		}
+
+		public override string ToString()
+		{
+			string allMatrix = "(";
+			for (int i = 0; i < Rows; i++)
+			{
+				string strokaForMatrix = "";
+				for (int j = 0; j < Colums-1; j++)
+				{
+					strokaForMatrix += _arrayForMatrix[i, j].ToString() + ", ";
+				}
+				strokaForMatrix += _arrayForMatrix[i, Colums - 1].ToString();
+				if (i != Rows - 1 && i == 0)
+				{
+					allMatrix += strokaForMatrix + "\n";
+				}
+				else if (i != Rows - 1 && i != 0)
+				{
+					allMatrix += " " + strokaForMatrix + "\n";
+				}
+				else
+				{
+					allMatrix += " " + strokaForMatrix + ")";
+				}//end of If-case
+			}
+			return allMatrix;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return (obj is Matrix)&&this.Equals((Matrix)obj);//call another bool Equals after check if it is comparable type
+		}
+
+		public bool Equals(Matrix matrix)//main Equal method
+		{
+			bool answerEqualOrNot = true;
+			if (Rows == matrix.Rows && Colums == matrix.Colums)
+			{
+				for (int i = 0; i < Rows-1; i++)
+				{
+					for (int j = 0; j < Colums-1; j++)
+					{
+						if (answerEqualOrNot == true)
+						{
+							answerEqualOrNot = _arrayForMatrix[i, j] == matrix._arrayForMatrix[i, j];
+						}
+						else
+						{
+							i = Rows - 1;
+							j = Colums - 1;
+						}
+					}
+				}
+			}
+			return answerEqualOrNot;
+		}
+
+		public override int GetHashCode()
+		{
+			return _arrayForMatrix.GetHashCode();
+		}
+
+		public bool IsSquared()
+		{
+			if (Rows == Colums)
+				return true;
+			else
+				return false;
+		}
+
+		public static bool CompareDimension(Matrix matrix1, Matrix matrix2)
+		{
+			if (matrix1.Rows == matrix2.Rows && matrix1.Colums == matrix2.Colums)
+				return true;
+			else
+				return false;
+		}
+
+		public static double Determinant(Matrix matrix)
+		{
+			double result = 0.0d;
+			if (!matrix.IsSquared())
+			{
+				throw new NotSquaredMatrix("The matrix must be squared!");
+			}
+			if (matrix.Rows == 1)
+				result = matrix[0, 0];
+			else
+			{
+				for (int i = 0; i < matrix.Rows; i++)
+				{
+					result += Math.Pow(-1, i) * matrix[0, i] * Determinant(Matrix.Minor(matrix, 0, i));
+				}
+			}
+			return result;
+		}
+
+		public static Matrix Minor(Matrix matrix, int row, int colums)
+		{
+			Matrix _newmatrix = new Matrix(matrix.Rows - 1, matrix.Colums - 1);
+			int ii = 0, jj = 0;
+			for (int i = 0; i < matrix.Rows; i++)
+			{
+				if (i == row)
+					continue;
+				jj = 0;
+				for (int j = 0; j < matrix.Colums; j++)
+				{
+					if (j == colums)
+						continue;
+					_newmatrix[ii, jj] = matrix[i, j];
+					jj++;
+				}
+				ii++;
+			}
+			return _newmatrix;
+		}
+
+		public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+		{
+			if (!Matrix.CompareDimension(matrix1, matrix2))
+			{
+				throw new NotTheSameDimensionsMatrixs("The dimensions of two matrices must be the same!");
+			}
+			Matrix result = new Matrix(matrix1.Rows, matrix1.Colums);
+			for (int i = 0; i < matrix1.Rows; i++)
+			{
+				for (int j = 0; j < matrix1.Colums; j++)
+				{
+					result[i, j] = matrix1[i, j] + matrix2[i, j];
+				}
+			}
+			return result;
+		}
+
+		public static Matrix operator -(Matrix matrix)
+		{
+			for (int i = 0; i < matrix.Rows; i++)
+			{
+				for (int j = 0; j < matrix.Colums; j++)
+				{
+					matrix[i, j] = -matrix[i, j];
+				}
+			}
+			return matrix;
+		}
+
+		public static Matrix operator -(Matrix matrix1, Matrix matrix2)
+		{
+			if (!Matrix.CompareDimension(matrix1, matrix2))
+			{
+				throw new NotTheSameDimensionsMatrixs("The dimensions of two matrices must be the same!");
+			}
+			Matrix result = new Matrix(matrix1.Rows, matrix1.Colums);
+			for (int i = 0; i < matrix1.Rows; i++)
+			{
+				for (int j = 0; j < matrix1.Colums; j++)
+				{
+					result[i, j] = matrix1[i, j] - matrix2[i, j];
+				}
+			}
+			return result;
+		}
+
+		public static Matrix operator *(Matrix matrix, double multiplier)
+		{
+			Matrix result = new Matrix(matrix.Rows, matrix.Colums);
+			for (int i = 0; i < matrix.Rows; i++)
+			{
+				for (int j = 0; j < matrix.Colums; j++)
+				{
+					result[i, j] = matrix[i, j] * multiplier;
+				}
+			}
+			return result;
+		}
+		
+		public static Matrix operator *(double multiplier, Matrix matrix)
+		{
+			Matrix result = new Matrix(matrix.Rows, matrix.Colums);
+			for (int i = 0; i < matrix.Rows; i++)
+			{
+				for (int j = 0; j < matrix.Colums; j++)
+				{
+					result[i, j] = matrix[i, j] * multiplier;
+				}
+			}
+			return result;
+		}
+
+		public static Matrix operator /(Matrix matrix, double divider)
+		{
+			Matrix result = new Matrix(matrix.Rows, matrix.Colums);
+			for (int i = 0; i < matrix.Rows; i++)
+			{
+				for (int j = 0; j < matrix.Colums; j++)
+				{
+					result[i, j] = matrix[i, j] / divider;
+				}
+			}
+			return result;
+		}
+
+		public static Matrix operator /(double divider, Matrix matrix2)
+		{
+			Matrix result = new Matrix(matrix2.Rows, matrix2.Colums);
+			for (int i = 0; i < matrix2.Rows; i++)
+			{
+				for (int j = 0; j < matrix2.Colums; j++)
+				{
+					result[i, j] = matrix2[i, j] / divider;
+				}
+			}
+			return result;
+		}
+
+		public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+		{
+			if (matrix1.Colums != matrix2.Rows)
+			{
+				throw new ColumnsNotEquelRows("The numbers of columns of the" +
+				 " first matrix must be equal to the number of " +
+				 " rows of the second matrix!");
+			}
+			double tmp;
+			Matrix result = new Matrix(matrix1.Rows, matrix2.Colums);
+			for (int i = 0; i < matrix1.Rows; i++)
+			{
+				for (int j = 0; j < matrix2.Colums; j++)
+				{
+					tmp = result[i, j];
+					for (int k = 0; k < result.Rows; k++)
+					{
+						tmp += matrix1[i, k] * matrix2[k, j];
+					}
+					result[i, j] = tmp;
+				}
+			}
+			return result;
+		}
+
+
+
+	}
+}
---
+++ Polynomial/Polynomial.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForPolynomial
+{
+	public class DegreeNotEqueltoLenghtException : ApplicationException
+	{
+		public DegreeNotEqueltoLenghtException() : base()
+		{ }
+
+		public DegreeNotEqueltoLenghtException(string message) : base(message)
+		{ }
+
+		public DegreeNotEqueltoLenghtException(string message, Exception innerException) : base(message, innerException)
+		{ }
+		
+	}
+	public class CanNotComparePolynomialException : ApplicationException
+	{
+		public CanNotComparePolynomialException() : base()
+		{ }
+
+		public CanNotComparePolynomialException(string message) : base(message)
+		{ }
+
+		public CanNotComparePolynomialException(string message, Exception innerException) : base(message, innerException)
+		{ }
+
+	}
+	public class InvalidCastException : ApplicationException
+	{
+		public InvalidCastException() : base()
+		{ }
+
+		public InvalidCastException(string message) : base(message)
+		{ }
+
+		public InvalidCastException(string message, Exception innerException) : base(message, innerException)
+		{ }
+
+	}
+	public class ZeroDegreeException : ApplicationException
+	{
+		public ZeroDegreeException() : base()
+		{ }
+
+		public ZeroDegreeException(string message) : base(message)
+		{ }
+
+		public ZeroDegreeException(string message, Exception innerException) : base(message, innerException)
+		{ }
+
+	}
+	public class DegreeIsChangedException : ApplicationException
+	{
+		public DegreeIsChangedException() : base()
+		{ }
+
+		public DegreeIsChangedException(string message) : base(message)
+		{ }
+
+		public DegreeIsChangedException(string message, Exception innerException) : base(message, innerException)
+		{ }
+
+	}
+	public class Polynomial : IComparable<Polynomial>
+	{
+		//bool minusDegree = false;
+		private double[] _coefficientsOfPoliomial;
+		int _hideDegree;
+		bool isDegreeZero = false;
+
+		public Polynomial()
+		{ }
+
+		public Polynomial(double[] coef)
+		{
+				DegreeOfPolinomial = coef.Length;
+				this._coefficientsOfPoliomial = coef;
+		}
+
+		public double[] CoefficientOfPoliomial
+		{
+			get
+			{
+				return _coefficientsOfPoliomial;
+			}
+			protected set
+			{
+				_coefficientsOfPoliomial = value;
+			}
+		}
+
+		public int DegreeOfPolinomial
+		{
+			get
+			{
+				return _hideDegree;
+			}
+			protected set
+			{
+				if (value == 0)
+				{
+					isDegreeZero = true;
+
+					
+				}
+
+				_hideDegree = value;
+			}
+		}
+
+		public double this[int index]//indexer
+		{
+			get { return _coefficientsOfPoliomial[index]; }
+			protected set
+			{
+				_coefficientsOfPoliomial[index] = value;
+			}
+
+		}
+
+		public void SetNewDegree(int degree, int coef)
+		{
+			if (degree == 0)
+			{
+				this[_coefficientsOfPoliomial.Length] = coef;
+			}
+			if (degree >= DegreeOfPolinomial)
+			{
+				Polynomial _newPolynomial = new Polynomial(new double[degree]);
+				for (int i = 0; i < DegreeOfPolinomial; i++)
+				{
+					_newPolynomial[i] = this[i];
+				}
+				_newPolynomial[degree] = coef;
+				DegreeOfPolinomial = degree;
+				_coefficientsOfPoliomial = _newPolynomial._coefficientsOfPoliomial;
+			}
+			else { throw new DegreeIsChangedException("SetDegree method trying change value on lower"); }
+		}
+		
+		public int CompareTo(Polynomial polynomial)
+		{
+
+			if (polynomial == null) { return 1; }
+			if (DegreeOfPolinomial == polynomial.DegreeOfPolinomial)
+			{
+				if (CoefficientOfPoliomial != polynomial.CoefficientOfPoliomial)
+				{
+					bool comparator = false;//predict safaty of this part of code
+					for (int i = 0; i < CoefficientOfPoliomial.Length; i++)
+					{
+
+						if (comparator == false)//safety
+						{
+							if (this[i] != polynomial[i])
+							{
+								comparator = true;//safety
+								return 1;
+							}
+							else { }
+						}
+					}
+				}
+				else if (CoefficientOfPoliomial == polynomial.CoefficientOfPoliomial)
+				{
+					return 0;
+				}
+
+			}
+			else if (DegreeOfPolinomial != polynomial.DegreeOfPolinomial)
+			{
+				return DegreeOfPolinomial.CompareTo(polynomial.DegreeOfPolinomial);
+			}
+			else
+			{
+				throw new CanNotComparePolynomialException();
+			}
+			return 1;
+		}
+
+		public int CompareTo(Object obj)
+		{
+			Polynomial polynomial;
+			if (obj is Polynomial)
+			{
+				polynomial = obj as Polynomial;
+			}
+			else
+			{
+				throw new InvalidCastException("You have sent a member to Polynomial that cannot be cast.");
+			}
+			if (polynomial != null)
+			{
+				return CompareTo(polynomial);
+			}
+			else { return 1; }
+		}
+		
+		public void ChangeCoefficient(int indexForChange, double substituteValueOfCoefficient)
+		{
+			this[indexForChange] = substituteValueOfCoefficient;
+		}
+
+		public override string ToString()
+		{
+			string stringPolynomial = "";
+			string sign = "+";
+			byte cheak = 0;
+			for (int i = 0; i < CoefficientOfPoliomial.Length; i++)
+			{
+				if (this.DegreeOfPolinomial >= 0)
+				{
+					if (this[i] < 0)
+					{
+						stringPolynomial += $"{this[i].ToString()}x^{this.DegreeOfPolinomial - i}";
+					}
+					else if (i < CoefficientOfPoliomial.Length - 1 && this[i] != 0)
+					{
+						stringPolynomial += $"{sign}{this[i].ToString()}x^{this.DegreeOfPolinomial - i}";
+					}
+					else if (i == CoefficientOfPoliomial.Length - 1 && this[i] != 0)
+					{
+						stringPolynomial += $"{sign}{this[i].ToString()}x";
+					}
+				}
+				else
+				{
+					if (isDegreeZero == true)
+					{
+						if (this[i] > 0)
+						{
+							stringPolynomial += $"{sign}{this[i].ToString()}";
+						}
+						else if (this[i] < 0)
+						{
+							sign = "-";
+							stringPolynomial += $"{sign}{this[i].ToString()}";
+						}
+						else { stringPolynomial = "0"; }
+
+					}
+					else
+					{
+						i--;
+						cheak++;
+						if (cheak > 2)
+						{
+							throw new ZeroDegreeException("It is invalid  convert to string, Because Degree could not be recognized ");
+						}
+					}
+				}
+
+				cheak = 0;
+				sign = "+";
+			}
+			return stringPolynomial;
+		}
+		public static bool CompareDegreeAndCoef(Polynomial polynom1, Polynomial polynom2)
+		{
+			if (polynom1.CoefficientOfPoliomial == polynom2.CoefficientOfPoliomial && polynom1.DegreeOfPolinomial == polynom2.DegreeOfPolinomial)
+				return true;
+			else
+				return false;
+		}
+		public static Polynomial operator +(Polynomial polyn1, Polynomial polyn2)
+		{
+			Polynomial _newPolynomial = default(Polynomial);
+			if (polyn1.CoefficientOfPoliomial.Length == polyn2.CoefficientOfPoliomial.Length)
+			{
+				_newPolynomial = new Polynomial(new double[polyn1.CoefficientOfPoliomial.Length]);
+				for (int i = 0; i < polyn1.CoefficientOfPoliomial.Length; i++)
+				{
+					_newPolynomial[i] = polyn1[i] + polyn2[i];
+				}
+				return _newPolynomial;
+			}
+			else if (polyn1.CoefficientOfPoliomial != polyn2.CoefficientOfPoliomial)
+			{
+
+				if (polyn1.DegreeOfPolinomial > polyn2.DegreeOfPolinomial)
+				{
+					return GetPolynomial(polyn1, polyn2);//polyn1>polyn2
+				}
+				else if (polyn1.DegreeOfPolinomial < polyn2.DegreeOfPolinomial)
+				{
+					return GetPolynomial(polyn2, polyn1);//polyn1<polyn2
+				}
+				else { throw new DegreeIsChangedException(""); }
+
+
+				Polynomial GetPolynomial(Polynomial inst1, Polynomial inst2)//First argument always must have bigger Coefficient and Degree
+				{
+					_newPolynomial = new Polynomial(new double[inst1.CoefficientOfPoliomial.Length]);
+					for (int i = 0; i < inst1.CoefficientOfPoliomial.Length; i++)
+					{
+						if (i > inst1.DegreeOfPolinomial - 1)
+						{
+							_newPolynomial[i] = inst1[i] + inst2[i];
+						}
+						else
+						{
+							_newPolynomial[i] = inst1[i];
+						}
+
+					}
+					return _newPolynomial;
+				}
+			}
+			else
+			{
+				throw new DegreeNotEqueltoLenghtException("corrupted operator + cause Degree is different, but coefficients is the same length");
+			}
+		}
+		public static Polynomial operator *(Polynomial polyn1, Polynomial polyn2)
+		{
+			Polynomial _newPolynomial = default(Polynomial);
+			if (CompareDegreeAndCoef(polyn1, polyn2)|| polyn1.DegreeOfPolinomial != polyn2.DegreeOfPolinomial && polyn1.CoefficientOfPoliomial != polyn2.CoefficientOfPoliomial)
+			{
+				_newPolynomial = new Polynomial(new double[(polyn1.DegreeOfPolinomial + polyn2.DegreeOfPolinomial)-2]);
+				for (int i = 0; i < polyn1.DegreeOfPolinomial; i++)
+				{
+					for (int index = 0; index < polyn2.DegreeOfPolinomial; index++)
+					{
+							_newPolynomial[i+index] += polyn1[i] * polyn2[index];
+					}
+					
+				}
+				return _newPolynomial;
+			}
+			else
+			{
+				throw new DegreeNotEqueltoLenghtException("corrupted operator * cause Degree is different, but coefficients is the same length");
+			}
+		}
+		public static Polynomial operator -(Polynomial polyn1, Polynomial polyn2)
+		{
+			Polynomial _newPolynomial = default(Polynomial);
+			if ( polyn1.CoefficientOfPoliomial.Length == polyn2.CoefficientOfPoliomial.Length)
+			{
+				_newPolynomial = new Polynomial(new double[polyn1.CoefficientOfPoliomial.Length]);
+				for (int i = 0; i < polyn1.CoefficientOfPoliomial.Length; i++)
+				{
+					_newPolynomial[i] = polyn1[i] - polyn2[i];
+				}
+				return _newPolynomial;
+			}
+			else if (polyn1.CoefficientOfPoliomial != polyn2.CoefficientOfPoliomial)
+			{
+				int _lenghtForPolynomial, _degreeForPolynomial;
+				
+				if (polyn1.DegreeOfPolinomial > polyn2.DegreeOfPolinomial)
+				{
+					_lenghtForPolynomial = polyn1.CoefficientOfPoliomial.Length;//polyn1>polyn2
+					_degreeForPolynomial = polyn1.DegreeOfPolinomial;
+
+
+				}
+				else if (polyn1.DegreeOfPolinomial < polyn2.DegreeOfPolinomial)
+				{
+					_lenghtForPolynomial = polyn2.CoefficientOfPoliomial.Length;//polyn1<polyn2
+					_degreeForPolynomial = polyn2.DegreeOfPolinomial;
+
+				}
+				else { throw new ApplicationException(""); }
+				return GetPolynomial(polyn1, polyn2, _lenghtForPolynomial, _degreeForPolynomial);
+
+				Polynomial GetPolynomial(Polynomial inst1, Polynomial inst2, int _lenth, int _degree)//First argument NOT always must have bigger Coefficient and Degree
+				{
+					_newPolynomial = new Polynomial( new double[_lenth]);
+					for (int i = 0; i < _lenth; i++)
+					{
+						if (i > inst1.DegreeOfPolinomial - 1)
+						{
+							_newPolynomial[i] = inst1[i] - inst2[i];
+						}
+						else
+						{
+							_newPolynomial[i] = inst1[i];
+						}
+
+					}
+					return _newPolynomial;
+				}
+
+			}
+			else
+			{
+				throw new ApplicationException("corrupted operator - cause Degree is different, but coefficients is the same length");
+			}
+
+
+
+		}
+		
+	}
+}
